98419f8d59074759482cde693b17853c8a1e282d
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2025-05-01 12:23:47 -0300

    Create an icons folder

---
+++ client/src/components/icons/plus-icon.tsx
@@ -0,0 +1,19 @@
+export default function PlusIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      width={32}
+      height={32}
+      strokeWidth={2}
+    >
+      <title>Crear nota</title>{" "}
+      <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path>{" "}
+      <path d="M9 12h6"></path> <path d="M12 9v6"></path>{" "}
+    </svg>
+  );
+}
