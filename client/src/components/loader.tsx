cf18b4b2c67351dfa46550cc79bd63e43599107a
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2025-05-01 12:22:31 -0300

    Create a loader component

---
+++ client/src/components/loader.tsx
@@ -0,0 +1,16 @@
+export default function Loader() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
