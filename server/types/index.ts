139282f08d9d727dfe5dc15f2d48f32e789b82c2
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2025-05-02 13:16:10 -0300

    Create the types folder

---
+++ server/types/index.ts
@@ -0,0 +1,11 @@
+export interface UserInterface {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface NoteInterface {
+  title: string;
+  content: string;
+  userId: string;
+}
